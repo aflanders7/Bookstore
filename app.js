423e30ed65bd58318be256173ebead766eed6a84
Audrey Flanders <114197067+aflanders7@users.noreply.github.com>
2023-08-02 11:24:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8000;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8034;                 // Set a port number at the top so it's easy to change in the future
 // Database
 var db = require('./db-connector')
 
