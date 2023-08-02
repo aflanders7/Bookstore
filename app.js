84e8ce06fb73a9dd8c04a224f9eba5d525601bfa
JacobOSU <97179651+JacobOSU@users.noreply.github.com>
2023-08-02 12:05:00 -0500

    step 5

--- app.js
+++ app.js
@@ -31,7 +31,46 @@ app.get('/', function(req, res)                 // This is the basic syntax for 
         res.render('index', {data: rows});
         })
     });                                 // requesting the web site.
+// app.js
 
+app.post('/add-merch-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let merchName = parseInt(data['input-merchName']);
+    if (isNaN(merchName))
+    {
+        merchName = 'NULL'
+    }
+
+    let merchPrice = parseInt(data['input-merchPrice']);
+    if (isNaN(merchPrice))
+    {
+        merchPrice = 'NULL'
+    }
+
+    let merchQuantity = parseInt(data['input-merchQuantity']);
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Merchandise (merchName, merchPrice, merchQuantity) VALUES ('${data['input-merchName']}', '${data['input-merchPrice']}', '${data['input-merchQuantity']}' ${merchName}, ${merchPrice}, ${merchQuantity})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+})
 /*
     LISTENER
 */
