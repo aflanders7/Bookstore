54c0308eb93d484fa1f8b5267a561246fd128279
Audrey Flanders <flandera@flip1.engr.oregonstate.edu>
2023-08-03 12:32:56 -0700

    delete merch

--- public/js/delete_merchandise.js
+++ public/js/delete_merchandise.js
@@ -1,40 +1,26 @@
-function deleteMerchandise(merchandiseID) {
-    // Put our data we want to send in a javascript object
+function deleteMerchandise(merchID) {
+    let link = '/delete-merchandise-ajax/';
     let data = {
-        merchID: merchandiseID
+      merchID: personID
     };
-
-    // Setup our AJAX request
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("DELETE", "/delete-merchandise-ajax", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-
-    // Tell our AJAX request how to resolve
-    xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 204) {
-
-            // Add the new data to the table
-            deleteRow(merchandiseID);
-
-        }
-        else if (xhttp.readyState == 4 && xhttp.status != 204) {
-            console.log("There was an error with the input.")
-        }
-    }
-    // Send the request and wait for the response
-    xhttp.send(JSON.stringify(data));
-}
-
-
-function deleteRow(merchandiseID){
-
-    let table = document.getElementById("merch-table");
-    for (let i = 0, row; row = table.rows[i]; i++) {
-       //iterate through rows
-       //rows would be accessed using the "row" variable assigned in the for loop
-       if (table.rows[i].getAttribute("data-value") == merchandiseID) {
-            table.deleteRow(i);
-            break;
-       }
-    }
-}
+  
+    $.ajax({
+      url: link,
+      type: 'DELETE',
+      data: JSON.stringify(data),
+      contentType: "application/json; charset=utf-8",
+      success: function(result) {
+        deleteRow(personID);
+      }
+    });
+  }
+  
+  function deleteRow(personID){
+      let table = document.getElementById("merch-table");
+      for (let i = 0, row; row = table.rows[i]; i++) {
+         if (table.rows[i].getAttribute("data-value") == personID) {
+              table.deleteRow(i);
+              break;
+         }
+      }
+  }
