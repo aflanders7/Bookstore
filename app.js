7a30e182cac1b510de9a01a81b32efdd42dce2dc
Audrey Flanders <flandera@flip1.engr.oregonstate.edu>
2023-08-02 22:12:01 -0700

    added ajax post req

--- app.js
+++ app.js
@@ -37,6 +37,48 @@ app.get('/', function(req, res)
     })
 });                                    // will process this file, before sending the finished HTML to the client.
 
+app.post('/add-merch-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values- no null values
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Merchandise (merchName, merchPrice, merchQuantity) VALUES ('${data.merchName}', '${data.merchPrice}', ${data.merchQuantity}`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT 
+            query2 = `SELECT * FROM merchandise;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
 app.post('/add-merch-form', function(req, res){
     // Capture the incoming data and parse it back to a JS object
     let data = req.body;
