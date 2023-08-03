d19224f7ac703692c7f9d8bc664844b94a3f8ac0
Audrey Flanders <flandera@flip1.engr.oregonstate.edu>
2023-08-02 21:23:47 -0700

    solving server issue

--- app.js
+++ app.js
@@ -6,14 +6,15 @@
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 const path = require('path');
+PORT        = 8034;                 
 
 // app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 // app.use(express.static('public'))
-PORT        = 8034;                 // Set a port number at the top so it's easy to change in the future
+
 
 // Database
-var db = require('./database/db-connector')
+var db = require('./database/db-connector');
 
 // Handlebars
 const { engine } = require('express-handlebars');
@@ -21,49 +22,57 @@ var exphbs = require('express-handlebars');
 app.engine('.hbs', engine({extname: ".hbs"}));
 app.set('view-engine', '.hbs');
 
-const { query } = require('express');
+// const { query } = require('express');
 
 
 /*
     ROUTES
 */
+// app.get('/', function(req, res)
+// {
+//     // Declare Query 1
+//     let query1;
+
+//     // If there is no query string, we just perform a basic SELECT
+//     if (req.query.merchName === undefined)
+//     {
+//         query1 = "SELECT * FROM Merchandise;";
+//     }
+
+//     // If there is a query string, we assume this is a search, and return desired results
+//     else
+//     {
+//         query1 = `SELECT * FROM Merchandise WHERE merchName LIKE "${req.query.merchName}%"`
+//     }
+
+//     // Query 2 is the same in both cases
+//     let query2 = "SELECT * FROM Merchandise;";
+
+//     // Run the 1st query
+//     db.pool.query(query1, function(error, rows, fields){
+        
+//         // Save the people
+//         let Merchandise = rows;
+        
+//         // Run the second query
+//         db.pool.query(query2, (error, rows, fields) => {
+            
+//             // Save the planets
+//             let Merch = rows;  //NOT SURE HOW TO IMPLEMENT THIS YET, NOT YET CORRECT
+
+//             return res.render('index', {data: Merchandise, Merch: Merch});
+//         })
+//     })
+// });                        // requesting the web site.
+// app.js
+
 app.get('/', function(req, res)
 {
-    // Declare Query 1
-    let query1;
-
-    // If there is no query string, we just perform a basic SELECT
-    if (req.query.merchName === undefined)
-    {
-        query1 = "SELECT * FROM Merchandise;";
-    }
-
-    // If there is a query string, we assume this is a search, and return desired results
-    else
-    {
-        query1 = `SELECT * FROM Merchandise WHERE merchName LIKE "${req.query.merchName}%"`
-    }
-
-    // Query 2 is the same in both cases
-    let query2 = "SELECT * FROM Merchandise;";
-
-    // Run the 1st query
+    let query1 = "SELECT * FROM Merchandise;";
     db.pool.query(query1, function(error, rows, fields){
-        
-        // Save the people
-        let Merchandise = rows;
-        
-        // Run the second query
-        db.pool.query(query2, (error, rows, fields) => {
-            
-            // Save the planets
-            let Merch = rows;  //NOT SURE HOW TO IMPLEMENT THIS YET, NOT YET CORRECT
-
-            return res.render('index', {data: Merchandise, Merch: Merch});
-        })
+    res.render('index', {data: rows});
     })
-});                        // requesting the web site.
-// app.js
+}); 
 
 app.get('/books', function (req, res,html) {
     res.sendFile(path.join(__dirname+'/views/books.html'));
@@ -95,44 +104,44 @@ app.get('/sales', function (req, res,html) {
 
 
 
-app.post('/add-merch-form', function(req, res){
-    // Capture the incoming data and parse it back to a JS object
-    let data = req.body;
+// app.post('/add-merch-form', function(req, res){
+//     // Capture the incoming data and parse it back to a JS object
+//     let data = req.body;
 
-    // Capture NULL values
-    let merchName = parseInt(data['input-merchName']);
-    if (isNaN(merchName))
-    {
-        merchName = 'NULL'
-    }
+//     // Capture NULL values
+//     let merchName = parseInt(data['input-merchName']);
+//     if (isNaN(merchName))
+//     {
+//         merchName = 'NULL'
+//     }
 
-    let merchPrice = parseInt(data['input-merchPrice']);
-    if (isNaN(merchPrice))
-    {
-        merchPrice = 'NULL'
-    }
+//     let merchPrice = parseInt(data['input-merchPrice']);
+//     if (isNaN(merchPrice))
+//     {
+//         merchPrice = 'NULL'
+//     }
 
-    let merchQuantity = parseInt(data['input-merchQuantity']);
-    // Create the query and run it on the database
-    query1 = `INSERT INTO Merchandise (merchName, merchPrice, merchQuantity) VALUES ('${data['input-merchName']}', '${data['input-merchPrice']}', '${data['input-merchQuantity']}' ${merchName}, ${merchPrice}, ${merchQuantity})`;
-    db.pool.query(query1, function(error, rows, fields){
+//     let merchQuantity = parseInt(data['input-merchQuantity']);
+//     // Create the query and run it on the database
+//     query1 = `INSERT INTO Merchandise (merchName, merchPrice, merchQuantity) VALUES ('${data['input-merchName']}', '${data['input-merchPrice']}', '${data['input-merchQuantity']}' ${merchName}, ${merchPrice}, ${merchQuantity})`;
+//     db.pool.query(query1, function(error, rows, fields){
 
-        // Check to see if there was an error
-        if (error) {
+//         // Check to see if there was an error
+//         if (error) {
 
-            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-            console.log(error)
-            res.sendStatus(400);
-        }
+//             // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+//             console.log(error)
+//             res.sendStatus(400);
+//         }
 
-        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
-        // presents it on the screen
-        else
-        {
-            res.redirect('/');
-        }
-    })
-})
+//         // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+//         // presents it on the screen
+//         else
+//         {
+//             res.redirect('/');
+//         }
+//     })
+// })
 /*
     LISTENER
 */
