1b8d369fdcace09e3cefd7659bc92485df59948e
aflanders7 <audrey.flanders117@gmail.com>
2023-08-07 21:02:17 -0700

    css

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'));
 const { query } = require('express');
-app.use(express.static('public'));
+
 /*
     ROUTES
 */
