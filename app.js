c638e40f2177defb2ced573abe7319fb3ffdf41c
aflanders7 <audrey.flanders117@gmail.com>
2023-08-08 19:35:24 -0700

    add employee and customer

--- app.js
+++ app.js
@@ -164,6 +164,60 @@ app.post('/add-book-form', function(req, res){
     })
 });
 
+app.post('/add-customer-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values- no null values
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Customers (customerName, customerEmail) VALUES ('${data['input-customerName']}', '${data['input-customerEmail']}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/customers');
+        }
+    })
+});
+
+app.post('/add-employee-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values- no null values
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Employees (employeeName, phoneNumber) VALUES ('${data['input-employeeName']}', '${data['input-phoneNumber']}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/employees');
+        }
+    })
+});
+
 ////////////// Update Operations
 
 app.post('/update-merch', function(req,res){
