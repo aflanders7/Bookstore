e5b6c531280c86bd929f2899c8b469c716f85bee
JacobOSU <97179651+JacobOSU@users.noreply.github.com>
2023-08-10 14:09:27 -0500

    Create delete_merchandise_sale.js

---
+++ public/js/delete_merchandise_sale.js
@@ -0,0 +1,40 @@
+function deleteMerchandiseSale(merchandiseSaleID) {
+  // Put our data we want to send in a javascript object
+  let data = {
+      merchSaleID: merchandiseSaleID
+  };
+
+  // Setup our AJAX request
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("DELETE", "/delete-merchandise-ajax", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  // Tell our AJAX request how to resolve
+  xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+          // Add the new data to the table
+          deleteRow(merchandiseSaleID);
+
+      }
+      else if (xhttp.readyState == 4 && xhttp.status != 204) {
+          console.log("There was an error with the input.")
+      }
+  }
+  // Send the request and wait for the response
+  xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(merchandiseID){
+
+  let table = document.getElementById("merch-table");
+  for (let i = 0, row; row = table.rows[i]; i++) {
+     //iterate through rows
+     //rows would be accessed using the "row" variable assigned in the for loop
+     if (table.rows[i].getAttribute("data-value") == merchandiseSaleID) {
+          table.deleteRow(i);
+          break;
+     }
+  }
+}
