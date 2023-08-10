d188e0a61175760067412f9ec76e4a919e06f717
JacobOSU <97179651+JacobOSU@users.noreply.github.com>
2023-08-10 14:22:37 -0500

    Create delete_book_sale.js

---
+++ public/js/delete_book_sale.js
@@ -0,0 +1,40 @@
+function deleteBookeSale(bookSaleID) {
+  // Put our data we want to send in a javascript object
+  let data = {
+      bookSaleID: bookSaleID
+  };
+
+  // Setup our AJAX request
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("DELETE", "/delete-book-ajax", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  // Tell our AJAX request how to resolve
+  xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+          // Add the new data to the table
+          deleteRow(bookSaleID);
+
+      }
+      else if (xhttp.readyState == 4 && xhttp.status != 204) {
+          console.log("There was an error with the input.")
+      }
+  }
+  // Send the request and wait for the response
+  xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(bookSaleID){
+
+  let table = document.getElementById("book-table");
+  for (let i = 0, row; row = table.rows[i]; i++) {
+     //iterate through rows
+     //rows would be accessed using the "row" variable assigned in the for loop
+     if (table.rows[i].getAttribute("data-value") == bookSaleID) {
+          table.deleteRow(i);
+          break;
+     }
+  }
+}
