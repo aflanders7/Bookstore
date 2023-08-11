f31a7ac4a54b43bfb2a17790704d95f1bc1ead64
aflanders7 <audrey.flanders117@gmail.com>
2023-08-11 13:00:03 -0700

    citations for app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// citations
+
+// Citation for the code setup and listener:
+// Date 8/11/23
+// Copied from OSU CS 340 nodejs-starter-app
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main
+
+// Citation for the read and search operations:
+// Date 8/11/23
+// Adapted from OSU CS 340 nodejs-starter-app step 4 and step 6, queries are entirely our own work
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%204%20-%20Dynamically%20Displaying%20Data
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%206%20-%20Dynamically%20Filling%20Dropdowns%20and%20Adding%20a%20Search%20Box
+
+// Citation for the add operations:
+// Date 8/11/23
+// Adapted from OSU CS 340 nodejs-starter-app step 5, queries are entirely our own work
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%205%20-%20Adding%20New%20Data
+
+// Citation for the delete operations:
+// Date 8/11/23
+// Adapted from OSU CS 340 nodejs-starter-app step 6, queries are entirely our own work
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%207%20-%20Dynamically%20Deleting%20Data
+
+// Citation for the update operations:
+// Date 8/11/23
+// Adapted from OSU CS 340 nodejs-starter-app step 8, queries are entirely our own work
+// Update functions were modified based on the add function post requests
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%208%20-%20Dynamically%20Updating%20Data
+
+
+
+
 // App.js
 
 /*
