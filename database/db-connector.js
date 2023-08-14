796c44b11caa006f1989e1b091d82109e89177d2
Audrey Flanders <114197067+aflanders7@users.noreply.github.com>
2023-08-14 10:53:19 -0400

    Sanitize db-connector

--- database/db-connector.js
+++ database/db-connector.js
@@ -6,10 +6,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_summejac',
-    password        : 'nMNLHParmmwo',
-    database        : 'cs340_summejac'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our applicaiton
