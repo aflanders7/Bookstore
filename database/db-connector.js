ab02c80fe8861eff54fb4d567ac75f9bbbe893d8
Audrey Flanders <114197067+aflanders7@users.noreply.github.com>
2023-08-14 10:56:07 -0400

    added citation

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,4 +1,8 @@
 // ./database/db-connector.js
+//citation
+// Citation for the DB connection: 
+// Date 8/11/23 Copied directly from OSU CS 340 nodejs-starter-app, but used our own connection credentials 
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main
 
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
