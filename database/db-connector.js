781b9b33fc58c31034ea87ff477cbd7894e92668
Audrey Flanders <114197067+aflanders7@users.noreply.github.com>
2023-08-21 15:56:01 -0400

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,19 +1,14 @@
-// ./database/db-connector.js
-//citation
-// Citation for the DB connection: 
-// Date 8/11/23 Copied directly from OSU CS 340 nodejs-starter-app, but used our own connection credentials 
-// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main
 
-// Get an instance of mysql we can use in the app
+// Get an instance of mysql to use in the app
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
-    user            : 'oracle',
-    password        : 'oraclepassword',
-    database        : 'oracledb'
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our applicaiton
